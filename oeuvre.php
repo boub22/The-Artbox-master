d7c3a69a3c36ae1ba948ee78064578f7b07cffcc
babs <bkoita122@gmail.com>
2025-01-03 20:28:06 +0100

    first commit

---
+++ oeuvre.php
@@ -0,0 +1,10 @@
+<?php
+
+$recipes = ['Dodomu', 'Couscous', 'Aashaah Baadaleen', 'Nightlife Traffic', 'Nightlife Traffic', 'Red Washover', 
+'Chromatics', 'Digital Negative', 'Blast from the past', 'Hurricane', 'La marée rouge', 'Asimilacion',
+'La Galaxia Gialla', 'Puffy Amalgamate', 'Mirage', 'Blaue Gelbe Muster',];
+
+<?php
+echo $recipes[1]; 
+
+?>
