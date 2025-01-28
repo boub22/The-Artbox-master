fc50e1f731d51a9045cade3e09b5138d2036c9d1
boub22 <bkoita122@gmail.com>
2025-01-28 16:51:08 +0100

    Update oeuvre.php

--- oeuvre.php
+++ oeuvre.php
@@ -1,16 +1,4 @@
-<!doctype html>
-<html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>The ArtBox</title>
-</head>
-
-<body>
     <!-- Intégration du header -->
     <?php require_once(__DIR__ . '../header.php'); ?>
     <main>
